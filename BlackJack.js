f3448583605bd7a4df6b8c0ed28807bfe52167ad
Andrea Macellaro La Franca <andrea.macellaro@gmail.com>
2024-11-25 14:42:45 +0100

    Adds card deal and flip sound

--- BlackJack.js
+++ BlackJack.js
@@ -14,42 +14,56 @@ function start() {
     let audio = document.getElementById("audio");
     let audioClick = document.getElementById("audio1");
     let music = document.getElementById("audio2");
+    let cardDealSound = document.getElementById("cardDealSound");
+    let cardFlip = document.getElementById("cardFlip");
 
     btnStart.addEventListener("click", function () {
         music.play();
-        music.volume = 0.1;
-        audioClick.play();
+        music.volume = 0.01;
+        // audioClick.play();
         btnStart.disabled = true;
         delayBtn(btnHit, btnStop, btnStand, btnRestart);
 
-        player = new Player();  
-        dealer = new Dealer();  
-        deck = new Deck();      
+        player = new Player();
+        dealer = new Dealer();
+        deck = new Deck();
         deck.shuffle();
 
         let card1 = deck.draw();
         img1 = document.createElement("img"); //PLAYER
-        img1.id ="img1"
+        img1.id = "img1"
         img1.src = `images/${card1.image}`;
-        setTimeout(() => 
-            playerCards.appendChild(img1), 1000); 
+        setTimeout(() =>
+            playerCards.appendChild(img1), 1000);
+        setTimeout(() =>
+            cardDealSound.play(), 1000);
+
         let card2 = deck.draw();
         img2 = document.createElement("img"); //DEALER
         img2.id = "img2"
         img2.src = `images/${card2.image}`;
-        setTimeout(() => 
-            dealerCards.appendChild(img2), 2000); 
+        setTimeout(() =>
+            dealerCards.appendChild(img2), 2000);
+        setTimeout(() =>
+            cardDealSound.play(), 2000);
+
         let card3 = deck.draw();
         img3 = document.createElement("img"); //PLAYER
         img3.id = "img3"
         img3.src = `images/${card3.image}`;
-        setTimeout(() => 
-            playerCards.appendChild(img3), 3000); 
+        setTimeout(() =>
+            playerCards.appendChild(img3), 3000);
+        setTimeout(() =>
+            cardDealSound.play(), 3000);
+
         img4 = document.createElement("img"); //DEALER BACK
         img4.id = "img4"
         img4.src = `images/Deck-Back.png`;
-        setTimeout(() => 
+        setTimeout(() =>
             dealerCards.appendChild(img4), 4000);
+        setTimeout(() =>
+            cardDealSound.play(), 4000);
+
         setTimeout(() => {
             dealer.addCard(card2);
             dScore.textContent = dealer.totalScoreWithAce();
@@ -61,7 +75,7 @@ function start() {
             pScore.textContent = player.totalScoreWithAce();
             playerScore.appendChild(pScore);
         }, 4000);
-        
+
         setTimeout(() => {
             if (player.totalScoreWithAce() == 21) {
                 btnHit.disabled = true;
@@ -84,6 +98,7 @@ function start() {
                         let img = document.createElement("img");
                         img.src = `images/${card.image}`;
                         dealerCards.appendChild(img);
+                        cardDealSound.play();
                         dScore.textContent = dealer.totalScoreWithAce();
                         dealerScore.innerHTML = "";
                         dealerScore.appendChild(dScore);
@@ -106,40 +121,55 @@ function start() {
     });
 
     btnRestart.addEventListener("click", function () {
-        audioClick.play();
+        // audioClick.play();
         audio.pause();
         deactivateBtn(btnHit, btnStop, btnStand, btnRestart);
         delayBtn(btnHit, btnStop, btnStand, btnRestart);
-        
-        playerCards.innerHTML = ""; 
-        dealerCards.innerHTML = ""; 
+
+        playerCards.innerHTML = "";
+        dealerCards.innerHTML = "";
         dealerScore.innerHTML = "";
         playerScore.innerHTML = "";
 
-        player = new Player();  
+        player = new Player();
         dealer = new Dealer();
-        deck = new Deck();      
+        deck = new Deck();
         deck.shuffle();
 
         let card1 = deck.draw();
         img1 = document.createElement("img"); //PLAYER
+        img1.id = "img1"
         img1.src = `images/${card1.image}`;
-        setTimeout(() => 
-            playerCards.appendChild(img1), 1000); 
+        setTimeout(() =>
+            playerCards.appendChild(img1), 1000);
+        setTimeout(() =>
+            cardDealSound.play(), 1000);
+
         let card2 = deck.draw();
         img2 = document.createElement("img"); //DEALER
+        img2.id = "img2"
         img2.src = `images/${card2.image}`;
-        setTimeout(() => 
-            dealerCards.appendChild(img2), 2000); 
+        setTimeout(() =>
+            dealerCards.appendChild(img2), 2000);
+        setTimeout(() =>
+            cardDealSound.play(), 2000);
+
         let card3 = deck.draw();
         img3 = document.createElement("img"); //PLAYER
+        img3.id = "img3"
         img3.src = `images/${card3.image}`;
-        setTimeout(() => 
-            playerCards.appendChild(img3), 3000); 
+        setTimeout(() =>
+            playerCards.appendChild(img3), 3000);
+        setTimeout(() =>
+            cardDealSound.play(), 3000);
+
         img4 = document.createElement("img"); //DEALER BACK
+        img4.id = "img4"
         img4.src = `images/Deck-Back.png`;
-        setTimeout(() => 
+        setTimeout(() =>
             dealerCards.appendChild(img4), 4000);
+        setTimeout(() =>
+            cardDealSound.play(), 4000);
 
         setTimeout(() => {
             dealer.addCard(card2);
@@ -175,6 +205,7 @@ function start() {
                         let img = document.createElement("img");
                         img.src = `images/${card.image}`;
                         dealerCards.appendChild(img);
+                        cardDealSound.play();
                         dScore.textContent = dealer.totalScoreWithAce();
                         dealerScore.innerHTML = "";
                         dealerScore.appendChild(dScore);
@@ -197,17 +228,18 @@ function start() {
     });
 
     btnHit.addEventListener("click", function () {
-        audioClick.play();
+        // audioClick.play();
         let card = deck.draw();
         player.addCard(card);
         let img = document.createElement("img");
         img.src = `images/${card.image}`;
         playerCards.appendChild(img);
+        cardDealSound.play();
         pScore.textContent = player.totalScoreWithAce();
         playerScore.innerHTML = "";
         playerScore.appendChild(pScore);
-        
-        if(player.totalScoreWithAce() == 21){
+
+        if (player.totalScoreWithAce() == 21) {
             btnHit.disabled = true;
             btnStand.disabled = true;
             pScore.textContent = `Points: ${player.totalScoreWithAce()}`;
@@ -218,6 +250,7 @@ function start() {
             dealer.addCard(card4);
             img4.src = `images/${card4.image}`;
             dealerCards.appendChild(img4);
+            cardFlip.play();
             dScore.textContent = dealer.totalScoreWithAce();
             dealerScore.innerHTML = "";
             dealerScore.appendChild(dScore);
@@ -228,24 +261,26 @@ function start() {
                     let img = document.createElement("img");
                     img.src = `images/${card.image}`;
                     dealerCards.appendChild(img);
+                    cardDealSound.play();
                     dScore.textContent = dealer.totalScoreWithAce();
                     dealerScore.innerHTML = "";
                     dealerScore.appendChild(dScore);
-                if(dealer.totalScoreWithAce() === 21){
-                    pScore.textContent = `Draw! Points: ${player.totalScoreWithAce()}`;
-                    playerScore.appendChild(pScore);
-                    dScore.textContent = `Draw! Points: ${dealer.totalScoreWithAce()}`;
-                    dealerScore.appendChild(dScore);
-                }else{
-                    audio.play();
-                    pScore.textContent = `Won! Points: ${player.totalScoreWithAce()}`;
-                    playerScore.appendChild(pScore);
-                    dScore.textContent = `Lost! Points: ${dealer.totalScoreWithAce()}`;
-                    dealerScore.appendChild(dScore);
+                    if (dealer.totalScoreWithAce() === 21) {
+                        pScore.textContent = `Draw! Points: ${player.totalScoreWithAce()}`;
+                        playerScore.appendChild(pScore);
+                        dScore.textContent = `Draw! Points: ${dealer.totalScoreWithAce()}`;
+                        dealerScore.appendChild(dScore);
+                    } else {
+                        audio.play();
+                        pScore.textContent = `Won! Points: ${player.totalScoreWithAce()}`;
+                        playerScore.appendChild(pScore);
+                        dScore.textContent = `Lost! Points: ${dealer.totalScoreWithAce()}`;
+                        dealerScore.appendChild(dScore);
+                    }
                 }
-            }}, 1000);
+            }, 1000);
         }
-        if(player.totalScoreWithAce()>21){
+        if (player.totalScoreWithAce() > 21) {
             btnHit.disabled = true;
             btnStand.disabled = true;
             pScore.textContent = `Bust! Points: ${player.totalScoreWithAce()}`;
@@ -256,6 +291,7 @@ function start() {
             dealer.addCard(card4);
             img4.src = `images/${card4.image}`;
             dealerCards.appendChild(img4);
+            cardFlip.play();
             dScore.textContent = dealer.totalScoreWithAce();
             dealerScore.innerHTML = "";
             dealerScore.appendChild(dScore);
@@ -266,31 +302,34 @@ function start() {
                     let img = document.createElement("img");
                     img.src = `images/${card.image}`;
                     dealerCards.appendChild(img);
+                    cardDealSound.play();
                     dScore.textContent = dealer.totalScoreWithAce();
                     dealerScore.innerHTML = "";
                     dealerScore.appendChild(dScore);
-            }}, 1000);
+                }
+            }, 1000);
             setTimeout(() => {
-                if(dealer.totalScoreWithAce()<21){
+                if (dealer.totalScoreWithAce() < 21) {
                     pScore.textContent = `Lost! Points: ${player.totalScoreWithAce()}`;
                     playerScore.innerHTML = "";
                     playerScore.appendChild(pScore);
                     dScore.textContent = `Won! Points: ${dealer.totalScoreWithAce()}`;
                     dealerScore.innerHTML = "";
                     dealerScore.appendChild(dScore);
-                }else if (dealer.totalScoreWithAce()>21){
+                } else if (dealer.totalScoreWithAce() > 21) {
                     pScore.textContent = `Draw! Points: ${player.totalScoreWithAce()}`;
                     playerScore.innerHTML = "";
                     playerScore.appendChild(pScore);
                     dScore.textContent = `Draw! Points: ${dealer.totalScoreWithAce()}`;
                     dealerScore.innerHTML = "";
                     dealerScore.appendChild(dScore);
-            }}, 1100);
+                }
+            }, 1100);
         }
     });
-    
-    btnStand.addEventListener("click", function(){
-        audioClick.play();
+
+    btnStand.addEventListener("click", function () {
+        // audioClick.play();
         dealerCards.removeChild(img4);
         btnHit.disabled = true;
         btnStand.disabled = true;
@@ -298,21 +337,23 @@ function start() {
         dealer.addCard(card4);
         img4.src = `images/${card4.image}`;
         dealerCards.appendChild(img4);
+        cardFlip.play();
         dScore.textContent = dealer.totalScoreWithAce();
         dealerScore.innerHTML = "";
         dealerScore.appendChild(dScore);
         while (dealer.totalScoreWithAce() < 16) {
-                let card = deck.draw();
-                dealer.addCard(card);
-                let img = document.createElement("img");
-                img.src = `images/${card.image}`;
-                dealerCards.appendChild(img);
-                dScore.textContent = dealer.totalScoreWithAce();
-                dealerScore.innerHTML = "";
-                dealerScore.appendChild(dScore);
+            let card = deck.draw();
+            dealer.addCard(card);
+            let img = document.createElement("img");
+            img.src = `images/${card.image}`;
+            dealerCards.appendChild(img);
+            cardDealSound.play();
+            dScore.textContent = dealer.totalScoreWithAce();
+            dealerScore.innerHTML = "";
+            dealerScore.appendChild(dScore);
         }
         setTimeout(() => {
-            if(player.totalScoreWithAce()>dealer.totalScoreWithAce() || dealer.totalScoreWithAce()>21){
+            if (player.totalScoreWithAce() > dealer.totalScoreWithAce() || dealer.totalScoreWithAce() > 21) {
                 audio.play();
                 pScore.textContent = `Won! Points: ${player.totalScoreWithAce()}`;
                 playerScore.innerHTML = "";
@@ -320,29 +361,30 @@ function start() {
                 dScore.textContent = `Lost! Points: ${dealer.totalScoreWithAce()}`;
                 dealerScore.innerHTML = "";
                 dealerScore.appendChild(dScore);
-            }else if(player.totalScoreWithAce()<dealer.totalScoreWithAce() && dealer.totalScoreWithAce()<=21){
+            } else if (player.totalScoreWithAce() < dealer.totalScoreWithAce() && dealer.totalScoreWithAce() <= 21) {
                 pScore.textContent = `Lost! Points: ${player.totalScoreWithAce()}`;
                 playerScore.innerHTML = "";
                 playerScore.appendChild(pScore);
                 dScore.textContent = `Won! Points: ${dealer.totalScoreWithAce()}`;
                 dealerScore.innerHTML = "";
                 dealerScore.appendChild(dScore);
-            }else{
+            } else {
                 pScore.textContent = `Draw! Points: ${player.totalScoreWithAce()}`;
                 playerScore.innerHTML = "";
                 playerScore.appendChild(pScore);
                 dScore.textContent = `Draw! Points: ${dealer.totalScoreWithAce()}`;
                 dealerScore.innerHTML = "";
                 dealerScore.appendChild(dScore);
-        }}, 1000);
+            }
+        }, 1000);
     });
-    
+
     btnStop.addEventListener("click", function () {
-        audioClick.play();
+        // audioClick.play();
         audio.pause();
         music.pause();
-        playerCards.innerHTML = ""; 
-        dealerCards.innerHTML = ""; 
+        playerCards.innerHTML = "";
+        dealerCards.innerHTML = "";
         dealerScore.innerHTML = "";
         playerScore.innerHTML = "";
         btnStart.disabled = false;
